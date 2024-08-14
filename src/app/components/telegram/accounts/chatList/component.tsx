83a030fefdd334d062af6ec2ec58517dd4905ab2
Yaroslav Khakhuda <hahyda14@gmail.com>
2024-08-14 14:06:03 +0300

    fix

--- src/app/components/telegram/accounts/chatList/component.tsx
+++ src/app/components/telegram/accounts/chatList/component.tsx
@@ -1,36 +1,3 @@
-import { ListGroup } from "react-bootstrap"
-export function ChatList(chats) {
-    return <>
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Subheading</div>
-          Cras justo odio
-        </div>
-      </ListGroup.Item>
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Subheading</div>
-          Cras justo odio
-        </div>
-        <Badge bg="primary" pill>
-          14
-        </Badge>
-      </ListGroup.Item>
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Subheading</div>
-          Cras justo odio
-        </div>
-
-      </ListGroup.Item>
-    </>
+export function ChatList() {
+    return <></>
 }
